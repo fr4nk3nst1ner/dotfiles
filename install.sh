c8ca52389541f8e1527da54877aa71a70a975fd6
Jonathan Stines <jonathan.stines@gmail.com>
2025-01-16 12:30:59 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,34 +19,26 @@ if [ -f /etc/os-release ]; then
                 git \
                 jq \
                 net-tools \
-                nmap \
-                zsh \
-                s3fs \
-                awscli \
-                tree \
-                ifmetric \
-                glances \
-                acl \
-                ansible
+                vim \
+                zsh
 
+        # Docker
         curl -fsSL https://get.docker.com | sudo bash
         sudo usermod -aG docker ubuntu
+
+        # oh-my-zsh
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
     fi
 fi
 
-cp ./.bash_profile $HOME/.bash_profile
-cp ./.vimrc $HOME/.vimrc
-mkdir $HOME/.vim_runtime
-cp -r ./vimrcs $HOME/.vim_runtime/
+cp ./.zprofile $HOME/.zprofile
 cp ./.zshrc $HOME/.zshrc
-#cp ./.tmux.conf $HOME/.tmux.conf
+cp ./.tmux.conf $HOME/.tmux.conf
 
-sudo cp ./.bash_profile /root/.bash_profile
-sudo cp ./.vimrc /root/.vimrc
-mkdir /root/.vim_runtime
-cp -r ./vimrcs /root/.vim_runtime/
-cp ./.zshrc /root/.zshrc
-#sudo cp ./.tmux.conf /root/.tmux.conf
+sudo cp ./.zprofile /root/.zprofile
+sudo cp ./.zshrc    /root/.zshrc
+sudo cp ./.tmux.conf /root/.tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
